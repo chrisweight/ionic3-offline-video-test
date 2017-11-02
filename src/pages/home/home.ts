2da073d419c1bd86e608875ccf06558bf1565d66
Christopher Weight <chris@chrisweight.com>
2017-11-02 15:19:51 +0000

    Final working PoC

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FileTransferObject, FileTransfer } from '@ionic-native/file-transfer';
 import { File } from '@ionic-native/file';
-import { DomSanitizer, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
 import { Platform, Content } from 'ionic-angular';
 import { ViewChild } from '@angular/core';
 import { normalizeURL } from 'ionic-angular';
@@ -17,27 +16,22 @@ export class HomePage {
   
   urls: string[] = [
     'http://f1.media.brightcove.com/4/1852113022001/1852113022001_1964328863001_pr-001-ch-05-sl-01.mp4?pubId=1852113022001&videoId=1964302431001',
-    'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
-    'http://localhost:8080/var/mobile/Containers/Data/Application/FD9BDB20-E922-4F08-A943-D85400214814/Library/NoCloud/BigBuckBunny_320x180.mp4'
+    'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'
   ];
 
   url: string;
-  src: string; // SafeUrl;
+  src: string;
   downloading: boolean = false;
   transfer: FileTransferObject;
   downloadButtonText: string = 'Download!';
 
   constructor(
     public navCtrl: NavController, 
-    public sanitizer: DomSanitizer,
     public platform: Platform,
     public fileTransfer: FileTransfer, 
     public file: File) {
 
-      this.url = this.urls[1];
-      // er....this works???
-      // this.src = this.urls[2];
-
+      this.url = this.urls[0];
       console.log(this.url);
 
       this.platform
@@ -73,6 +67,7 @@ checkfileExists() {
 onTap($event) {
     console.log('HomePage.onTap()', $event);
 
+    this.src = null;
     this.downloading = true;
     this.downloadButtonText = 'Downloading!';
 
@@ -89,16 +84,7 @@ onTap($event) {
 
   onComplete(entry) {
     this.downloadButtonText = 'Downloaded!';
-
-    // So this _should_ work, according to https://ionicframework.com/docs/wkwebview/. Doesn't.
-    let _path =  normalizeURL(entry.toURL());
-    console.log('normalised entry.toURL(): ', _path);
-     this.src = _path; 
-    // --
-
-    // This doesn't like to work either.
-    // this.src = this.sanitizer.bypassSecurityTrustUrl(_path); 
-
+    this.src = normalizeURL(entry.toURL()); 
     console.log('onComplete!', entry, this.src);  
   }
 
